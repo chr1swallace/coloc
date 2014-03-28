85298b1bf0f963fef93774b94eb835b619d9ea8f
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2014-03-28 14:45:17 +0000

    changed coeff.plot to use ggplot code

--- R/plot.R
+++ R/plot.R
@@ -64,7 +64,7 @@ abf.plot <- function(  coloc.obj, Pos=1:nrow(coloc.obj@results),
 
 
 
-coeff.plot <- function(b1,b2,s1,s2,eta,add=NULL, ...) {
+coeff.plot <- function(b1,b2,s1,s2,eta,add=NULL,alpha=NULL,slope=NULL, ...) {
   c1 <- cbind(b1,sqrt(s1),b1+1.96*sqrt(s1),b1-1.96*sqrt(s1))
   c2 <- cbind(b2,sqrt(s2),b2+1.96*sqrt(s2),b2-1.96*sqrt(s2))
   if(!is.null(add)) {
@@ -81,26 +81,49 @@ coeff.plot <- function(b1,b2,s1,s2,eta,add=NULL, ...) {
     yr[1] <- -0.005
   if(yr[2]<0)
     yr[2] <- 0.005
-  plot(c1[,1],c2[,1],pch=".",xlim=xr,ylim=yr, ...)
-  T <- seq(0,2,by=0.05)*pi
-  for(j in 1:nrow(c1)) {
-    X <- c1[j,1] + 1.96*c1[j,2]*cos(T)
-    Y <- c2[j,1] + 1.96*c2[j,2]*sin(T)
-    polygon(X,Y,lty=0,col="grey90")
-  }
-  for(j in 1:nrow(c1)) {
-    X <- c1[j,1] + 1.96*c1[j,2]*cos(T)
-    Y <- c2[j,1] + 1.96*c2[j,2]*sin(T)
-    polygon(X,Y,lty=3)
-  }
-  abline(0,eta,col="green",lwd=1.5)
-  abline(v=0,lty=2,col="green4",lwd=1.5)
-  abline(h=0,lty=2,col="green4",lwd=1.5)
-  arrows(c1[,3,drop=FALSE],c2[,1,drop=FALSE],c1[,4,drop=FALSE],c2[,1,drop=FALSE],angle=90,length=0.02,code=3)
-  arrows(c1[,1,drop=FALSE],c2[,3,drop=FALSE],c1[,1,drop=FALSE],c2[,4,drop=FALSE],angle=90,length=0.02,col="red",code=3)
-  points(c1[,1,drop=FALSE],c2[,1,drop=FALSE],pch="*",cex=1)
-  if(!is.null(add)) {
-    points(add[[1]],add[[2]],pch="*",cex=2,col="blue")
-    segments(c1[,5],c2[,5],c1[,1],c2[,1],col="blue",lty=3)
-  }
+
+  ## ggplot version
+  df <- data.frame(x=c1[,1], y=c2[,1],x.se=c1[,2],y.se=c2[,2],id=1:nrow(c1))
+  if(!is.null(alpha))
+    df$alpha <- alpha
+   T <- seq(0,2,by=0.05)*pi
+  df.path <- do.call("rbind",lapply(1:nrow(c1), function(j) {
+   tmp <- data.frame(x=c1[j,1] + 1.96*c1[j,2]*cos(T),
+                     y=c2[j,1] + 1.96*c2[j,2]*sin(T))
+   tmp$id <- j
+   return(tmp)
+  }))
+
+  p <- ggplot(df,aes(x=x,y=y,xmin=x-1.96*x.se,xmax=x+1.96*x.se,ymin=y-1.96*y.se,ymax=y+1.96*y.se,alpha=alpha)) +
+    geom_hline() + geom_vline() +
+      geom_errorbar(col="steelblue4") + 
+      geom_errorbarh(col="steelblue4") +theme(legend.position="none") + labs(x="beta.1",y="beta.2")
+  if(!is.null(slope))
+    p <- p + geom_abline(slope=slope,colour="blue",linetype="dashed")
+  p
+  
+  return(p)
+  
+##   plot(c1[,1],c2[,1],pch=".",xlim=xr,ylim=yr, ...)
+
+##   for(j in 1:nrow(c1)) {
+##     X <- c1[j,1] + 1.96*c1[j,2]*cos(T)
+##     Y <- c2[j,1] + 1.96*c2[j,2]*sin(T)
+##     polygon(X,Y,lty=0,col="grey90")
+##   }
+##   for(j in 1:nrow(c1)) {
+##     X <- c1[j,1] + 1.96*c1[j,2]*cos(T)
+##     Y <- c2[j,1] + 1.96*c2[j,2]*sin(T)
+##     polygon(X,Y,lty=3)
+##   }
+##   abline(0,eta,col="green",lwd=1.5)
+##   abline(v=0,lty=2,col="green4",lwd=1.5)
+##   abline(h=0,lty=2,col="green4",lwd=1.5)
+##   arrows(c1[,3,drop=FALSE],c2[,1,drop=FALSE],c1[,4,drop=FALSE],c2[,1,drop=FALSE],angle=90,length=0.02,code=3)
+##   arrows(c1[,1,drop=FALSE],c2[,3,drop=FALSE],c1[,1,drop=FALSE],c2[,4,drop=FALSE],angle=90,length=0.02,col="red",code=3)
+##   points(c1[,1,drop=FALSE],c2[,1,drop=FALSE],pch="*",cex=1)
+##   if(!is.null(add)) {
+##     points(add[[1]],add[[2]],pch="*",cex=2,col="blue")
+##     segments(c1[,5],c2[,5],c1[,1],c2[,1],col="blue",lty=3)
+##   }
 }
