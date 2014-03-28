6855214d140c9ea43b4451da112c5bad96309eba
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2014-03-28 14:44:39 +0000

    added a plot method for objects of class coloc

--- R/AllMethods.R
+++ R/AllMethods.R
@@ -16,3 +16,16 @@ setMethod("plot", signature(x="colocABF",y="missing"),
             abf.plot(x, ...)
           })
   
+#' @rdname plot-methods
+#' @aliases plot,coloc,missing-method
+setMethod("plot", signature(x="coloc",y="missing"),
+          function(x,...) {
+            coeff.plot(b1=x@plot.data$coef1,
+                       b2=x@plot.data$coef2,
+                       s1=sqrt(x@plot.data$var1),
+                       s2=sqrt(x@plot.data$var2),
+                       alpha=x@plot.data$model.prob,
+                       slope=x@result[["eta.hat"]],
+                       ...)
+          })
+  
