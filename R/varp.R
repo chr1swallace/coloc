efd03e72128386d47c5aac2742cf42ece9a9cf2f
Chris Wallace <cew54@cam.ac.uk>
2021-12-06 12:07:34 +0000

    added beginning of vignette

--- R/varp.R
+++ R/varp.R
@@ -1,23 +1,29 @@
+
 #' estimate prior probability of snp causality for gene expression based on
 #' distance from TSS
+#' @importFrom mgcv PredictMat
+#' @importFrom scam Predict.matrix.mpd.smooth
 #' @param dist distance in bp from TSS for an possible eqtl
+#' @param prior_at_mb prior value at dist=1e+6 (1 mega base). default is 1e-4,
+#'   which is the default value for p1 in coloc.abf
 #' @author Chrs Wallace
 #' @examples
 #' dist=seq(0,1e+6,by=1000)
 #' p=eqtl_prior(dist)
 #' plot(dist,p,type="l")
-#' @importFrom @mgcv PredictMat
-eqtl_prior=function(dist) {
+eqtl_prior=function(dist, prior_at_mb=1e-4) {
   np <- length(dist)
-  data <- data.frame(dist_bin=dist)
-  X <- PredictMat(eqtl_prior_data$smooth[[1]], data)
-  if (!is.matrix(X))
-    X<- matrix(X, nrow = nrow(data))
-  fit <- X %*% eqtl_prior_data$coefficients.t
-  linkinv <- eqtl_prior_data$family$linkinv
-  as.vector(linkinv(fit))
-}
-
-p1_p2_to_p12=function(d1, d2) {
-  p12
+  ## dist[ dist==0 ]=0.01
+  predict_base=function(dist) {
+    data <- data.frame(dist_bin=abs(dist))
+    X <- PredictMat(eqtl_prior_data$smooth[[1]], data)
+    if (!is.matrix(X))
+      X<- matrix(X, nrow = nrow(data))
+    fit <- X %*% eqtl_prior_data$coefficients.t
+    linkinv <- eqtl_prior_data$family$linkinv
+    as.vector(linkinv(fit))
+  }
+  sc=predict_base(1e+6)
+  pre=predict_base(dist)
+  pre*prior_at_mb/sc
 }
