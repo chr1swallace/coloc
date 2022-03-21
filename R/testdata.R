03b24ffcbdeac97060b4164ebb0e25a63266bb19
Chris Wallace <cew54@cam.ac.uk>
2022-03-21 10:50:47 +0000

    updated test data

--- R/testdata.R
+++ R/testdata.R
@@ -42,33 +42,49 @@ if(FALSE) {
     X2 <- simx(nsamples,S,maf)
     X3 <- simx(nsamples,S,maf)
     Y1 <- rnorm(nsamples,rowSums(X1[,causals[1:ncausals[1]],drop=FALSE]/2),sd.y[1])
+
     Y2 <- rnorm(nsamples,rowSums(X2[,causals[1:ncausals[2]],drop=FALSE]/2),sd.y[2])
     colnames(X1) <- colnames(X2) <- paste("s",1:nsnps,sep="")
     df1 <- cbind(Y=Y1,X1)
     df2 <- cbind(Y=Y2,X2)
-    ## if(nsim==1) {
+    return(list(
+      df1=as.data.frame(df1),
+      df2=as.data.frame(df2),
+      X3=X3,
+      causals=causals))
+  }
+
+  sim.null <- function(nsnps=50,nsamples=200,ncausals=0,sd.y=c(1,1.2)) {
+    cat("Generate a small set of data\n")
+    ntotal <- nsnps * nsamples
+    ## S <- toeplitz(sample(1:10,nsnps,replace=TRUE)/10)
+    S <- toeplitz((nsnps:1)/nsnps)
+    R=rWishart(1,2*nsnps,S)[,,1]
+    ## (1 - (abs(outer(1:nsnps,1:nsnps,`-`))/nsnps))
+    maf=runif(nsnps,0.2,0.8)^2
+    X1 <- simx(nsamples,S,maf)
+    X2 <- simx(nsamples,S,maf)
+    X3 <- simx(nsamples,S,maf)
+    Y1 <- rnorm(nsamples,sd.y[1])
+    Y2 <- rnorm(nsamples,sd.y[2])
+    colnames(X1) <- colnames(X2) <- paste("s",1:nsnps,sep="")
+    df1 <- cbind(Y=Y1,X1)
+    df2 <- cbind(Y=Y2,X2)
     return(list(#new("simdata",
       df1=as.data.frame(df1),
       df2=as.data.frame(df2),
       X3=X3))
-    ## } else {
-    ##   index <- split(1:(nsamples * nsim), rep(1:nsim, nsamples))
-    ##   objects <- lapply(index, function(i) list(#new("simdata",
-    ##                                          df1=as.data.frame(df1[i,]),
-    ##                                          df2=as.data.frame(df2[i,])))
-    ##   return(objects)
-    ## }
   }
+
 
   get.beta <- function(x,nm) {
     beta <- sapply(x,"[",1)
     varbeta <- sapply(x, "[", 2)^2
-    names(beta) <- names(varbeta) <- colnames(LD)
+    names(beta) <- names(varbeta) <- nm
     return(list(beta=beta,varbeta=varbeta))
   }
 
-  set.seed(46411)
-  data=sim.data(nsamples=1000,nsnps=50,sd.y=c(1.1,1),ncausals=c(1,1))
+make_data=function(data) {
   Y1 <- data$df1$Y
   Y2 <- data$df2$Y
   X1 <- as.matrix(data$df1[,-1])
@@ -85,7 +101,7 @@ if(FALSE) {
   b1 <- get.beta(tests1,colnames(LD))
   b2 <- get.beta(tests2,colnames(LD))
 
-  D1 <- list(beta=b1$beta,
+return(list(D1=list(beta=b1$beta,
              varbeta=b1$varbeta,
              N=nrow(X1),
              sdY=sd(Y1),
@@ -93,8 +109,8 @@ if(FALSE) {
              MAF=maf,
              LD=LD,
              snp=names(b1$beta),
-             position=1:nsnp)
-  D2 <- list(beta=b2$beta,
+             position=1:nsnp),
+  D2=list(beta=b2$beta,
              varbeta=b2$varbeta,
              N=nrow(X2),
              sdY=sd(Y2),
@@ -102,43 +118,26 @@ if(FALSE) {
              MAF=maf,
              LD=LD,
              snp=names(b2$beta),
-             position=1:nsnp)
+          position=1:nsnp)))
+}
 
-  data=sim.data(nsamples=1000,nsnps=50,sd.y=c(1.2,1.3),ncausals=c(2,1))
-  Y1 <- data$df1$Y
-  Y2 <- data$df2$Y
-  X1 <- as.matrix(data$df1[,-1])
-  X2 <- as.matrix(data$df2[,-1])
-  tests1 <- lapply(1:ncol(X1), function(i) summary(lm(Y1 ~ X1[,i]))$coefficients[2,])
-  tests2 <- lapply(1:ncol(X2), function(i) summary(lm(Y2 ~ X2[,i]))$coefficients[2,])
 
-  snpnames=make.unique(colnames(X2))
-  maf <- colMeans(data$X3)/2
-  names(maf) <- snpnames
-  LD <- cor(data$X3)
-  nsnp=length(maf)
-  dimnames(LD)=list(snpnames,snpnames)
-  b1 <- get.beta(tests1,colnames(LD))
-  b2 <- get.beta(tests2,colnames(LD))
-
-  D3 <- list(beta=b1$beta,
-             varbeta=b1$varbeta,
-             N=nrow(X1),
-             sdY=sd(Y1),
-             type="quant",
-             MAF=maf,
-             LD=LD,
-             snp=names(b1$beta),
-             position=1:nsnp)
-  D4 <- list(beta=b2$beta,
-             varbeta=b2$varbeta,
-             N=nrow(X2),
-             sdY=sd(Y2),
-             type="quant",
-             MAF=maf,
-             LD=LD,
-             snp=names(b2$beta),
-             position=1:nsnp)
+  ## set.seed(46411)
+  set.seed(42)
+  data=sim.data(nsamples=1000,nsnps=500,sd.y=c(1.1,1),ncausals=c(1,1))
+  data$causals # 105
+D1=make_data(data)[[1]]
+  D2=make_data(data)[[2]]
+  par(mfrow=c(2,1)); plot_dataset(D1); abline(v=data$causals); plot_dataset(D2); abline(v=data$causals)
+  data=sim.data(nsamples=1000,nsnps=500,sd.y=c(1.2,1.3),ncausals=c(2,1))
+  data$causals # 84, 379
+  D3=make_data(data)[[1]]
+D4=make_data(data)[[2]]
+  par(mfrow=c(2,1)); plot_dataset(D3); abline(v=data$causals); plot_dataset(D4); abline(v=data$causals)
+  null=sim.null(nsamples=1000,nsnps=500,sd.y=c(1.1,1))
+N1=make_data(null)[[1]]
+N2=make_data(null)[[2]]
+  par(mfrow=c(2,1)); plot_dataset(N1) ; plot_dataset(N2);
 
   par(mfrow=c(2,2))
   plot_dataset(D1,main="D1")
@@ -162,7 +161,7 @@ if(FALSE) {
   summary(runsusie(D5,coverage=0.1))  # lower coverage
   summary(runsusie(D5,coverage=0.01)) # even lower
 
-  coloc_test_data=list(D1=D1,D2=D2,D3=D3,D4=D4)
+  coloc_test_data=list(D1=D1,D2=D2,D3=D3,D4=D4,N1=N1,N2=N2)
   save(coloc_test_data, file="data/coloc_test_data.rda", version=2)
 
 }
